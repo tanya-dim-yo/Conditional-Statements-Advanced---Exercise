8fd0c533e4477d772334a01687ab8b834bd911f2
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-05 04:39:08 +0300

    Add files via upload

---
+++ 01. Cinema/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string projectionType = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            int seats = rows * cols;
+            double income = 0;
+            if (projectionType == "Premiere")
+            {
+                income = seats * 12.0;
+            }
+            else if (projectionType == "Normal")
+            {
+                income = seats * 7.50;
+            }
+            else if (projectionType == "Discount")
+            {
+                income = seats * 5.00;
+            }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
