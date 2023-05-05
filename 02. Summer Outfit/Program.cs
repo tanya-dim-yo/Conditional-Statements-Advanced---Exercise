6d6752cc3a2e4cd954846c03a28aa46b239c3a92
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-05 04:39:57 +0300

    Add files via upload

---
+++ 02. Summer Outfit/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Unicode;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+            if (timeOfDay == "Morning")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees >= 25)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (timeOfDay == "Afternoon")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (degrees >= 25)
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else if (timeOfDay == "Evening")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees > 18 && degrees <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees >= 25)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
