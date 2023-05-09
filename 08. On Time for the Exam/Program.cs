cf899e84b03cfd8e27c1d5fb55e673ed24e14357
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-09 05:05:11 +0300

    Add files via upload

---
+++ 08. On Time for the Exam/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OnTimeForTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHours = int.Parse(Console.ReadLine());
+            int examMinutes = int.Parse(Console.ReadLine());
+            int arrivalHours = int.Parse(Console.ReadLine());
+            int arrivalMinutes = int.Parse(Console.ReadLine());
+            string late = "Late";
+            string onTime = "On time";
+            string early = "Early";
+            int examTime = (examHours * 60) + examMinutes;
+            int arrivalTime = (arrivalHours * 60) + arrivalMinutes;
+            int totalMinutesDifference = arrivalTime - examTime;
+            string studentArrival = late;
+            if (totalMinutesDifference < -30)
+            {
+                studentArrival = early;
+            }
+            else if (totalMinutesDifference <= 0)
+            {
+                studentArrival = onTime;
+            }
+            string result = string.Empty;
+            if (totalMinutesDifference != 0)
+            {
+                int hoursDifference = Math.Abs(totalMinutesDifference / 60);
+                int minutesDifference = Math.Abs(totalMinutesDifference % 60);
+                if (hoursDifference > 0)
+                {
+                    result = string.Format("{0}:{1:00} hours", hoursDifference, minutesDifference);
+                }
+                else
+                {
+                    result = minutesDifference + " minutes";
+                }
+                if (totalMinutesDifference < 0)
+                {
+                    result += " before the start";
+                }
+                else
+                {
+                    result += " after the start";
+                }
+            }
+            Console.WriteLine(studentArrival);
+            if (!string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
